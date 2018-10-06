df00d034342983ea7998ed3f08d699f1028f8913
TofekHamodii <43014320+TofekHamodii@users.noreply.github.com>
2018-10-06 17:31:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '.'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`#Tx 4 Ever`,"http://twitch.tv/S-F")
+client.user.setGame(`#TxNetwork 🐦`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
