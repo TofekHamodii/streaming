309418e76b1994fd8031f5380cec4e31023491d5
Simple <43014320+TofekHamodii@users.noreply.github.com>
2018-11-14 20:52:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var prefix = "-";
  
  client.on('message', async message => {
   if(message.content.startsWith(prefix + "تقديم")) {
-    await message.channel.send("**الاسم**").then(e => {
+    await message.channel.send("**اسمك؟**").then(e => {
     let filter = m => m.author.id === message.author.id
     let lan = '';
     let md = '';
@@ -14,13 +14,13 @@ var prefix = "-";
       lan = collected.first().content
       collected.first().delete()
 e.delete();
-     message.channel.send('**العمر**').then(m => {
+     message.channel.send('**عمرك ؟**').then(m => {
 let chaMd = message.channel.awaitMessages(filter, { max: 1, time: 40000, errors: ['time'] })
 .then(co => {
   md = co.first().content
         co.first().delete()
         m.delete();
-message.channel.send('**ماذا تستطيع ان تقدم للسيرفر**').then(ms => {
+message.channel.send('**كيف تقدر تساعدنا بالاداره ؟**').then(ms => {
 let br = message.channel.awaitMessages(filter, { max: 1, time: 40000, errors: ['time'] })
 .then(col => {
   br = col.first().content
